eee117aae4c6aae9a795c24743761e3dfaf604e3
Emily Morgan <emorgan05@gmail.com>
2017-10-30 14:23:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       binding.pry
       item = @@items.find { |i| i.name = item_name }
-      
+
       if item != nil
         resp.write "Item not found"
         resp.status = 400
