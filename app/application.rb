e6a2c8862e933eb65be0ba474c6672101179b57e
Emily Morgan <emorgan05@gmail.com>
2017-10-30 14:18:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,14 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path == "/songs"
-      
+      item_name = req.path.split("/items/").last
+      item = @@items.find { |i| i.name = item_name }
+      resp.write item.price
     else
       resp.write "Route not found"
       resp.status = 404
     end
-    
+
     resp.finish
   end
 end
