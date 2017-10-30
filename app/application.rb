1dd113db744f9799cfaef6f4b3e85e0be518466f
Emily Morgan <emorgan05@gmail.com>
2017-10-30 14:23:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,9 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == "/songs"
+    if req.path == "/items"
       item_name = req.path.split("/items/").last
-      binding.pry
+      
       item = @@items.find { |i| i.name = item_name }
 
       if item != nil
