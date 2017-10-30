42467685781899e5e3deb1e333704d85a472d815
Emily Morgan <emorgan05@gmail.com>
2017-10-30 14:31:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    binding.pry
+
     if req.path.match("/items/")
       item_name = req.path.split("/items/").last
       item = @@items.find { |i| i.name == item_name }
@@ -16,7 +16,7 @@ class Application
       else
         resp.write item.price
       end
-      
+
     else
       resp.write "Route not found"
       resp.status = 404
