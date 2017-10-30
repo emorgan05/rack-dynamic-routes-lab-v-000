4b2d02408fe267a9964504a92f82b6734a5056b9
Emily Morgan <emorgan05@gmail.com>
2017-10-30 14:23:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path == "/items"
       item_name = req.path.split("/items/").last
-      
+
       item = @@items.find { |i| i.name = item_name }
 
       if item != nil
