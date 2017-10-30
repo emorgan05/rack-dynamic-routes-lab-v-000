b22e8182fb1570dfd74dc4c272e92cb8343a265f
Emily Morgan <emorgan05@gmail.com>
2017-10-30 14:16:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,15 @@
 class Application
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path == "/songs"
+      
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
     
     resp.finish
   end
